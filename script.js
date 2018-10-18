5b3b371f6805f8fd63f5eca7675c9e382bca89ca
WahiMac10 <wahimac10@WahiMac10s-MacBook-Air.local>
2018-10-18 00:33:04 -0400

    changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let fruit = ["Blueberry"];
+let fruit = [" Blueberry "," Apple "," Banana "];
 document.getElementById("fruitsH1").innerHTML = fruit;
 
 function addFruitToEnd() {
 	fruit.push(document.getElementById("fruitId").value);
 	console.log(fruit);
 	document.getElementById("fruitsH1").innerHTML = fruit.join(" with ");
-	document.getElementById("fruitsH1").innerHTML = fruit.sort();
+	
 
 }
